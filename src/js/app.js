4c5f4f98e1af7384e0534a8a9844d14cd037be19
leadd <ddeal812@gmail.com>
2017-07-06 21:02:16 -0500

    Add model and viewmodel, create list view

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
+//Data model
+var locations = [
+    {title: 'Campuzano Fine Mexican Food', location: {lat:32.482586,lng:-96.99423370000001}},
+    {title: 'Ellis County BBQ', location: {lat:32.476594,lng:-96.98378099999999}},
+    {title: 'Mockingbird Nature Park', location: {lat:32.4980242,lng:-96.96453369999999}},
+    {title: 'Big Cigar Racing', location: {lat:32.5052189,lng:-96.91816799999999}},
+    {title: 'Midlothian Heritage High School', location: {lat:32.4846421,lng:-96.9440862}}
+]
+
+var ViewModel = function() {
+    //Forces the content to stay within the viewmodel
+    var that = this;
+
+    //This will hold the objects in the locations array. The observable array will
+    //automatically update any bound DOM elements whenever the array changes.
+    this.locList = ko.observableArray([]);
+
+    //Push the locations objects to the observable array.
+    locations.forEach(function(loc) {
+        that.locList.push(loc);
+    });
+
+    this.currentLoc = ko.observable(this.locList()[0]);
+}
+
 //Variable to hold the map
 var map;
 
@@ -9,3 +34,5 @@ function initMap() {
         mapTypeControl: true
     });
 }
+
+ko.applyBindings(new ViewModel());
