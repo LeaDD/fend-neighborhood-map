9996b8e5795621dfc3372176b0521f3d592446aa
leadd <ddeal812@gmail.com>
2017-07-08 23:31:32 -0500

    More CSS, start working on markers array

--- src/js/app.js
+++ src/js/app.js
@@ -7,22 +7,6 @@ var locations = [
     {title: 'Midlothian Heritage High School', location: {lat:32.4846421,lng:-96.9440862}}
 ];
 
-var ViewModel = function() {
-    //Forces the content to stay within the viewmodel
-    var that = this;
-
-    //This will hold the objects in the locations array. The observable array will
-    //automatically update any bound DOM elements whenever the array changes.
-    this.locList = ko.observableArray([]);
-
-    //Push the locations objects to the observable array.
-    locations.forEach(function(loc) {
-        that.locList.push(loc);
-    });
-
-    this.currentLoc = ko.observable(this.locList()[0]);
-};
-
 //Variable to hold the map
 var map;
 
@@ -33,6 +17,53 @@ function initMap() {
         zoom: 13,
         mapTypeControl: true
     });
+
 }
+
+var ViewModel = function() {
+
+    var largeInfoWindow = new google.maps.InfoWindow();
+    //Forces the content to stay within the viewmodel
+    var that = this;
+
+    //This will hold the objects in the locations array. The observable array will
+    //automatically update any bound DOM elements whenever the array changes.
+    this.locList = ko.observableArray([]);
+
+    var markers = [];
+    //Style the markers a bit
+    //var defaultIcon = makeMarkerIcon('0091ff');
+
+    //Push the locations objects to the observable array.
+    locations.forEach(function(loc) {
+        that.locList.push(loc);
+    });
+
+
+    for(var i = 0; i < this.locList().length; i++) {
+        //Get position and title from the locList array
+        var position = this.locList()[i].location;
+        var title = this.locList()[i].title;
+
+        //Create marker per location and put into markers array
+        var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            //icon: defaultIcon,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+
+        markers.push(marker);
+    }
+
+    for (var j = 0; j < markers.length; j++) {
+        console.log(markers[j]);
+    }
+
+
+    this.currentLoc = ko.observable(this.locList()[0]);
+};
+
 
 ko.applyBindings(new ViewModel());
